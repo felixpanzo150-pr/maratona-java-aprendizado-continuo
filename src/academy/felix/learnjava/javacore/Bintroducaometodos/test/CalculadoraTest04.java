eb878c1f7b776ca722e97773eb0525ccf66258fd
felixpanzo150-pr <felixpanzo150@gmail.com>
2025-08-29 19:25:21 +0100

    feature:   Parãmetros tipo primitivos

---
+++ src/academy/felix/learnjava/javacore/Bintroducaometodos/test/CalculadoraTest04.java
@@ -0,0 +1,16 @@
+package academy.felix.learnjava.javacore.Bintroducaometodos.test;
+
+import academy.felix.learnjava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest04 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int num1 = 1;
+        int num2 = 2;
+        calculadora.alteraDoisNumeros(num1, num2);
+        System.out.println("Dentro CalculadoraTest04");
+        System.out.println("Num1 "+num1);
+        System.out.println("Num2 "+num2);
+
+    }
+}
